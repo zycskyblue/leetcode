a5e88b60cb0e916f2395e3e0fdcae694043ff53c
yuecheng <yuecheng.zhang@intel.com>
2013-12-13 22:38:25 +0800

    init on lva

---
+++ src/Leetcode/MergeIntervals.java
@@ -0,0 +1,20 @@
+package Leetcode;
+
+import java.util.ArrayList;
+
+public class MergeIntervals {
+
+	public class Interval {
+		int start;
+	    int end;
+	    Interval() { start = 0; end = 0; }
+	    Interval(int s, int e) { start = s; end = e; }
+	}
+	
+    public ArrayList<Interval> merge(ArrayList<Interval> intervals) {
+    	
+    	
+    	
+		return intervals;
+    }
+}
---
+++ src/Leetcode/WordLadder2.java
@@ -0,0 +1,18 @@
+package Leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class WordLadder2 {
+
+    public ArrayList<ArrayList<String>> findLadders(String start, String end, HashSet<String> dict) {
+		return null;
+        
+    }
+    
+    public class LadderNode{
+        public String value;
+        public int index;
+        public Boolean visited;
+    }
+}
